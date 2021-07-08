1c026472eaa1e21a85e6f5cab4a00a432bf475d0
Whelisson Dias <69944450+ULxD@users.noreply.github.com>
2021-07-08 18:39:12 -0300

    Add files via upload

---
+++ radio_simple_nui/ui.js
@@ -0,0 +1,16 @@
+function pvd(){
+	$("#mainmenu").css("display", "none");
+	$("#menuspvd").css("display", "block");
+}
+
+function opens(){
+	$("#mainmenu").css("display", "none");
+	$("#menusopen").css("display", "block");
+}
+
+function reset(){
+	$("#mainmenu").css("display", "block");
+	$("#menusopen").css("display", "none");
+	$("#menuspvd").css("display", "none");
+}
+
